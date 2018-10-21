65a38eb6808aca33155a1b6d4c6f51db1294a5d2
stevenhorkey <code@stevenhorkey.com>
2018-10-21 14:48:06 -0700

    creating new instance

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.send('error');
+  console.log(res);
+  console.log(err);
 });
 
 module.exports = app;
-
