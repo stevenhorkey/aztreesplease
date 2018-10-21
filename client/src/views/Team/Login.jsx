d44f2c8c52b0584cdbc12db68b3a0fa56315b8d6
stevenhorkey <code@stevenhorkey.com>
2018-10-21 14:34:47 -0700

    creating new instance

--- client/src/views/Team/Login.jsx
+++ client/src/views/Team/Login.jsx
@@ -49,25 +49,22 @@ class Login extends React.Component{
                 <Redirect to='/team' />
             )
         } else{
-            return(
+            return (
                 <div className='container p-5 text-justify'>
-                
-                <div className='white-modal col-md-6 p-5 mx-auto' id='login'>
-                    <h1 className='text-center mb-5 text-uppercase display-3 mt-2'><strong>Login</strong></h1>
-    
-                    <div className=''>
-                        <p className='mx-auto text-center'>Please login if you are a team member and would like to access additional content or <Link to={'/signup'}><a>sign up here</a></Link>...</p>
-                        <form className='mx-auto'>
-                            <input className='w-100 d-block my-3 p-1' type='text' name='email' onChange={this.handleChange} value={this.state.email} placeholder='Email' required/>
-                            <input className='w-100 d-block my-3 p-1' type='password' name='password' onChange={this.handleChange} value={this.state.password} placeholder='Password' required/>
-                            <p className='w-100 d-block mb-3 p-1 text-center small text-red'>{this.state.message}</p>
-                            <button className='w-100 d-block my-3 p-2' onClick={this.login.bind(this)}>Submit</button>
-                        </form>
+                    <div className='white-modal col-md-6 p-5 mx-auto' id='login'>
+                        <h1 className='text-center mb-5 text-uppercase display-3 mt-2'><strong>Login</strong></h1>
+        
+                        <div className=''>
+                            <p className='mx-auto text-center'>Please login if you are a team member and would like to access additional content or <Link to={'/signup'}><a>sign up here</a></Link>...</p>
+                            <form className='mx-auto'>
+                                <input className='w-100 d-block my-3 p-1' type='text' name='email' onChange={this.handleChange} value={this.state.email} placeholder='Email' required/>
+                                <input className='w-100 d-block my-3 p-1' type='password' name='password' onChange={this.handleChange} value={this.state.password} placeholder='Password' required/>
+                                <p className='w-100 d-block mb-3 p-1 text-center small text-red'>{this.state.message}</p>
+                                <button className='w-100 d-block my-3 p-2' onClick={this.login.bind(this)}>Submit</button>
+                            </form>
+                        </div>
                     </div>
-    
                 </div>
-                </div>
-                
             )
         }
         
